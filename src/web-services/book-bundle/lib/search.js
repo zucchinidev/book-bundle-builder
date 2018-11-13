48c95479af9dfa5cf650b2f78abb52a84ed830b8
Andrea Zucchini García <azga84@gmail.com>
2018-11-13 19:47:29 +0100

    end-point to retrieve suggest by term

--- src/web-services/book-bundle/lib/search.js
+++ src/web-services/book-bundle/lib/search.js
@@ -4,21 +4,17 @@ const request = require('request')
 
 const elasticsearchConfiguration = {}
 
-function getElasticsearchReqBody (req) {
-  return {
-    size: 10,
-    query: {
-      match: {
-        [req.params.field]: req.params.query
-      }
-    }
-  }
-}
-
 module.exports = (app, { host, port, books_index } = elasticsearchConfiguration) => {
   const url = `http://${host}:${port}/${books_index}/book/_search`
   app.get('/api/search/books/:field/:query', (req, res) => {
-    const body = getElasticsearchReqBody(req)
+    const body = {
+      size: 10,
+      query: {
+        match: {
+          [req.params.field]: req.params.query
+        }
+      }
+    }
     const options = { url, json: true, body }
     request.get(options, (err, esRes, esResBody) => {
       if (err) {
@@ -34,4 +30,36 @@ module.exports = (app, { host, port, books_index } = elasticsearchConfiguration)
       res.status(200).json(esResBody.hits.hits.map(({ _source }) => _source))
     })
   })
+
+  app.get('/api/suggest/:field/:query', (req, res) => {
+    const body = {
+      size: 0, //  Setting the size parameter to zero informs Elasticsearch that we don’t want any matching documents returned, just the suggestions
+      suggest: {
+        suggestions: {
+          text: req.params.query,
+          term: {
+            field: req.params.field,
+            suggest_mode: 'always',
+          },
+        }
+      }
+    }
+    const options = { url, json: true, body }
+    new Promise((resolve, reject) => {
+      request.get(options, (err, esRes, esResBody) => {
+        if (err) {
+          reject({ error: err })
+          return
+        }
+        if (esRes.statusCode !== 200) {
+          reject({ error: esResBody })
+          return
+        }
+        resolve(esResBody)
+      })
+    })
+      .then(esResBody => res.status(200).json(esResBody.suggest.suggestions))
+      .catch(({ error }) => res.status(error.status || 502).json(error))
+  })
+
 }
