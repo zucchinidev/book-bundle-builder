fdef15007a4b9934d814ac56c3e3d66336857adb
Andrea Zucchini García <azga84@gmail.com>
2018-11-16 20:59:10 +0100

    Creating end-point to create book bundles

---
+++ src/web-services/book-bundle/lib/bundle.js
@@ -0,0 +1,24 @@
+'use strict'
+const request = require('request-promise')
+const { required } = require('./RequiredParamException')
+
+function getUrl ({ host = required('host'), port = required('port'), bundles_index = required('bundles_index') }) {
+  return `http://${host}:${port}/${bundles_index}/bundle`
+}
+
+function createBundle (esConf) {
+  return async function (req, res) {
+    try {
+      const name = req.query.name || ''
+      const bundle = { name, books: [] }
+      const esRes = await request.post({ url: getUrl(esConf), body: bundle, json: true })
+      res.status(201).json(esRes)
+    } catch (err) {
+      res.status(err.statusCode || 502).json(err.error)
+    }
+  }
+}
+
+module.exports = (app, esConf) => {
+  app.post('/api/bundle', createBundle(esConf))
+}
