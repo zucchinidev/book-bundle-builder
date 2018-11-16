4ea498adb9d25c2f07a1f22d8e09652366eeb7ee
Andrea Zucchini García <azga84@gmail.com>
2018-11-16 22:10:01 +0100

    Exporting general url to manage books

--- src/web-services/book-bundle/lib/search.js
+++ src/web-services/book-bundle/lib/search.js
@@ -3,8 +3,12 @@
 const request = require('request-promise')
 const { required } = require('./RequiredParamException')
 
-function getUrl ({ host = required('host'), port = required('port'), books_index = required('books_index') }) {
-  return `http://${host}:${port}/${books_index}/book/_search`
+function getSearchUrl (esConf) {
+  return `${getBookUrl(esConf)}/_search`
+}
+
+function getBookUrl ({ host = required('host'), port = required('port'), books_index = required('books_index') }) {
+  return `http://${host}:${port}/${books_index}/book`
 }
 
 function getHitsByField (esConf) {
@@ -14,7 +18,7 @@ function getHitsByField (esConf) {
         size: 10,
         query: { match: { [req.params.field]: req.params.query } }
       }
-      const esRes = await request.get({ url: getUrl(esConf), json: true, body })
+      const esRes = await request.get({ url: getSearchUrl(esConf), json: true, body })
       res.status(200).json(esRes.hits.hits.map(({ _source }) => _source))
     } catch (err) {
       res.status(err.statusCode || 502).json(err.error)
@@ -33,7 +37,7 @@ function getSuggestionsByField (esConf) {
           suggestions: { text, term: { field, suggest_mode: suggestMode } }
         }
       }
-      const esResBody = await request.get({ url: getUrl(esConf), json: true, body })
+      const esResBody = await request.get({ url: getSearchUrl(esConf), json: true, body })
       res.status(200).json(esResBody.suggest.suggestions)
     } catch (err) {
       res.status(err.statusCode || 502).json(err.error)
@@ -45,3 +49,5 @@ module.exports = (app, esConf) => {
   app.get('/api/search/books/:field/:query', getHitsByField(esConf))
   app.get('/api/suggest/:field/:query', getSuggestionsByField(esConf))
 }
+
+module.exports.getBookUrl = esConf => getBookUrl(esConf)
