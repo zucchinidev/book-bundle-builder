9770363aa0e68e761c79e80dd5ea8bfa5add7624
Andrea Zucchini García <azga84@gmail.com>
2018-11-16 22:10:28 +0100

    Creating end-point to add book in bundle

--- src/web-services/book-bundle/lib/bundle.js
+++ src/web-services/book-bundle/lib/bundle.js
@@ -1,5 +1,6 @@
 'use strict'
 const request = require('request-promise')
+const getBookUrl = require('./search').getBookUrl
 const { required } = require('./RequiredParamException')
 
 function getUrl ({ host = required('host'), port = required('port'), bundles_index = required('bundles_index') }) {
@@ -39,7 +40,33 @@ function setBundleName (esConf) {
       const bundle = (await request(options))._source
       bundle.name = req.params.name
       const esRes = await request.put({ ...options, body: bundle })
-      res.status(209).json(esRes)
+      res.status(200).json(esRes)
+    } catch (err) {
+      res.status(err.statusCode || 502).json(err.error)
+    }
+  }
+}
+
+function insertBookInBundle (esConf) {
+  return async function (req, res) {
+    try {
+      const url = `${getUrl(esConf)}/${req.params.id}`
+      const bookId = req.params.pgid
+      const bookUrl = `${getBookUrl(esConf)}/${bookId}`
+      const options = { json: true }
+      const [{ _source: bundle, _version: version }, { _source: book }] = await Promise.all([
+        request({ ...options, url }),
+        request({ ...options, url: bookUrl })
+      ])
+
+      const bookNotPresentInBundle = bundle.books.findIndex(book => book.id === bookId) === -1
+
+      if (bookNotPresentInBundle) {
+        bundle.books.push({ id: bookId, title: book.title })
+      }
+
+      const esRes = await request.put({ ...options, body: bundle, url, qs: { version } })
+      res.status(200).json(esRes)
     } catch (err) {
       res.status(err.statusCode || 502).json(err.error)
     }
@@ -50,4 +77,5 @@ module.exports = (app, esConf) => {
   app.post('/api/bundle', createBundle(esConf))
   app.get('/api/bundle/:id', getBundleById(esConf))
   app.put('/api/bundle/:id/name/:name', setBundleName(esConf))
+  app.put('/api/bundle/:id/book/:pgid', insertBookInBundle(esConf))
 }
