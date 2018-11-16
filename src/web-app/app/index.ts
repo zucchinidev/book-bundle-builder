4af2aef7e90ac4f9945fa6d3e2b3ceed3b5cc716
Andrea Zucchini García <azga84@gmail.com>
2018-11-16 23:39:47 +0100

    Installing and using handlebars to create templates

--- src/web-app/app/index.ts
+++ src/web-app/app/index.ts
@@ -3,10 +3,10 @@ import 'bootstrap'
 import * as templates from './templates'
 const $ = document.body.querySelector.bind(document.body)
 
-document.body.innerHTML = templates.main
+document.body.innerHTML = templates.main()
 
 const mainElement = $('.b4-main')
 const alertsElement = $('.b4-alerts')
 
-mainElement.innerHTML = templates.welcome
-alertsElement.innerHTML = templates.alert
+mainElement.innerHTML = templates.welcome()
+alertsElement.innerHTML = templates.alert({type: 'info', message: 'Handlebars is working!!'})
--- src/web-app/app/templates.ts
+++ src/web-app/app/templates.ts
@@ -1,23 +1,25 @@
-export const main = `
+import * as Handlebars from '../node_modules/handlebars/dist/handlebars'
+
+export const main = Handlebars.compile(`
 <div class="container"> 
   <h1>B4 - Book Bundler</h1>
   <div class="b4-alerts"></div> 
   <div class="b4-main"></div> 
 </div>
-`
+`)
 
-export const welcome = `
+export const welcome = Handlebars.compile(`
 <div class="jumbotron">
   <h1>Welcome!</h1>
   <p>B4 is an application for creating book bundles.</p>  
 </div>
-`
+`)
 
-export const alert = `
-<div class="alert alert-success alert-dismissable fade in" role="alert">
+export const alert = Handlebars.compile(`
+<div class="alert alert-{{type}} alert-dismissable fade in" role="alert">
   <button class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span> 
   </button>
-  <strong>Success!</strong> Bootstrap is working.
+  {{message}}
 </div>
-`
+`)
