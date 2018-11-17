793ccacaaaba682741cccad1b034261c99cd69da
Andrea Zucchini García <azga84@gmail.com>
2018-11-17 12:43:25 +0100

    Use FileStorage method in express session middleware when the environment is development

--- src/web-app/server.js
+++ src/web-app/server.js
@@ -4,6 +4,11 @@ const express = require('express')
 const morgan = require('morgan')
 const nconf = require('nconf')
 const pkg = require('./package')
+const expressSession = require('express-session')
+
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackConfig = require('./webpack.config')
 
 nconf
   .argv()
@@ -23,14 +28,25 @@ const port = serviceUrl.port || (serviceUrl.protocol === 'https' ? 443 : 80)
 
 const app = express()
 app.use(morgan('dev'))
+
+if (isDev) {
+  const FileStore = require('session-file-store')(expressSession)
+  app.use(expressSession({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'unguessable',
+    store: new FileStore()
+  }))
+} else {
+
+}
+
 app.get('/api/version', (req, res) => res.status(200).send(pkg.version))
 require('./lib/search')(app, conf)
 require('./lib/bundle')(app, conf)
 
 if (isDev) {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackConfig = require('./webpack.config')
+
   app.use(webpackDevMiddleware(webpack(webpackConfig), {
     publicPath: '/',
     stats: { colors: true }
